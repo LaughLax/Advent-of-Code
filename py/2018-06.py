ff48446a6ba380f490c41cd45825b2956c54c482
David Haralson <d.m.haralson@ieee.org>
2018-12-08 03:11:52 -0700

    Performance improvements

--- py/2018-06.py
+++ py/2018-06.py
@@ -13,13 +13,14 @@ class AdventOfCode:
         return self.map.largest_region().area()
 
     def part2(self):
+        self.map.build_central_region()
         return len(self.map.central_region)
 
 
 class Map:
 
     def __init__(self):
-        self.regions = set()
+        self.regions = []
         self.min_x = None
         self.max_x = None
         self.min_y = None
@@ -28,49 +29,70 @@ class Map:
         self.central_region = set()
 
     def add_coordinate(self, xy):
-        self.regions.add(Region(xy, len(self.regions)))
+        self.regions.append(Region(xy, len(self.regions)))
 
     def find_region(self, x, y):
-        close_sort = sorted(self.regions, key=lambda p: p.distance_to_center(x, y))
-        return close_sort[0] if close_sort[0].distance_to_center(x, y) < close_sort[1].distance_to_center(x, y) else None
+        distances = [(x - r.x if x > r.x else r.x - x) + (y - r.y if y > r.y else r.y - y) for r in self.regions]
+
+        d = min(distances)
+        return self.regions[distances.index(d)] if distances.count(d) == 1 else None
 
     def total_distance(self, x, y):
         return sum(list(map(lambda r: r.distance_to_center(x, y), self.regions)))
 
+    def find_bounding_box(self):
+        self.min_x = float('inf')
+        self.max_x = 0
+        self.min_y = float('inf')
+        self.max_y = 0
+
+        for r in self.regions:
+            self.min_x = self.min_x if self.min_x < r.x else r.x
+            self.max_x = self.max_x if self.max_x > r.x else r.x
+
+            self.min_y = self.min_y if self.min_y < r.y else r.y
+            self.max_y = self.max_y if self.max_y > r.y else r.y
+
     def fill_map(self):
-        sorted_x = sorted(self.regions, key=lambda p: p.x)
-        self.min_x = sorted_x[0].x
-        self.max_x = sorted_x[-1].x
+        if self.min_x is None:
+            self.find_bounding_box()
 
-        sorted_y = sorted(self.regions, key=lambda p: p.y)
-        self.min_y = sorted_y[0].y
-        self.max_y = sorted_y[-1].y
-
-        for x in range(self.max_x - self.min_x + 1):
-            x = x + self.min_x
-            for y in range(self.max_y - self.min_y + 1):
-                y = y + self.min_y
-
-                if self.total_distance(x, y) < 10000:
-                    self.central_region.add((x,y))
+        for x in range(self.min_x+1, self.max_x):
+            for y in range(self.min_y+1, self.max_y):
 
                 region = self.find_region(x, y)
-                if not region or region.border_region:
-                    continue
+                if region:
+                    region.add_point(x, y)
 
-                region.add_point(x, y)
-
-                if (x == self.min_x
-                        or x == self.max_x
-                        or y == self.min_y
-                        or y == self.max_y):
+        for y in range(self.min_y, self.max_y + 1):
+            for x in [self.min_x, self.max_x]:
+                region = self.find_region(x, y)
+                if region:
                     region.border_region = True
 
-    def largest_region(self):
-        if self.biggest_region:
-            return self.biggest_region
+        for x in range(self.min_x, self.max_x + 1):
+            for y in [self.min_y, self.max_y]:
+                region = self.find_region(x, y)
+                if region:
+                    region.border_region = True
 
-        return sorted(self.regions, key=lambda r: r.area())[-1]
+    def build_central_region(self):
+        if self.min_x is None:
+            self.find_bounding_box()
+
+        for x in range(self.min_x, self.max_x + 1):
+            for y in range(self.min_y, self.max_y + 1):
+
+                if self.total_distance(x, y) < 10000:
+                    self.central_region.add((x, y))
+
+    def largest_region(self):
+        if not self.biggest_region:
+            self.biggest_region = self.regions[0]
+            for r in self.regions:
+                self.biggest_region = self.biggest_region if self.biggest_region.area() > r.area() else r
+
+        return self.biggest_region
 
 
 class Region:
@@ -88,12 +110,10 @@ class Region:
         self.furthest_distance = -1
 
     def distance_to_center(self, x, y):
-        return abs(self.x - x) + abs(self.y - y)
+        return (x - self.x if x > self.x else self.x - x) + (y - self.y if y > self.y else self.y - y)
 
     def add_point(self, x, y):
         self.points.add((x, y))
-        if self.distance_to_center(x, y) > self.furthest_distance:
-            self.furthest_distance = self.distance_to_center(x, y)
 
     def area(self):
         if self.border_region:
