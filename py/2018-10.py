dea0fa3cf1cb3ff13e6c8006f4b2b25311e75279
David Haralson <d.m.haralson@ieee.org>
2018-12-09 23:32:43 -0700

    Year 2018 Day 10

---
+++ py/2018-10.py
@@ -0,0 +1,70 @@
+class AdventOfCode:
+
+    def __init__(self, filename):
+        with open(filename) as f:
+            self.input = f.read().splitlines()
+
+        self.point_map = PointSet()
+        for line in self.input:
+            x = int(line[10:16])
+            y = int(line[18:24])
+            dx = int(line[36:38])
+            dy = int(line[40:42])
+
+            self.point_map.add_point(Point(x, y, dx, dy))
+
+    def part1(self):
+        t = 0
+        while True:
+            bounds = self.point_map.bounding_box(t)
+            if (bounds[2] - bounds[0]) <= 100 and (bounds[3] - bounds[1]) <= 12:
+                break
+            t += 1
+
+        self.point_map.print(t)
+
+        return t
+
+    def part2(self):
+        pass
+
+
+class PointSet:
+
+    def __init__(self):
+        self.points = []
+
+    def add_point(self, point):
+        self.points.append(point)
+
+    def bounding_box(self, t):
+        return (
+            min(pt.get_x(t) for pt in self.points),
+            min(pt.get_y(t) for pt in self.points),
+            max(pt.get_x(t) for pt in self.points),
+            max(pt.get_y(t) for pt in self.points),
+        )
+
+    def print(self, t):
+        points = [(pt.get_x(t), pt.get_y(t)) for pt in self.points]
+        bounds = self.bounding_box(t)
+        # rows = [0] * (bounds[2] - bounds[0] + 1)
+        for y in range(bounds[1], bounds[3] + 1):
+            row = [((x, y) in points) for x in range(bounds[0], bounds[2] + 1)]
+            print(''.join(['#' if i else '.' for i in row]))
+
+
+class Point:
+
+    def __init__(self, x, y, dx, dy):
+        self.x = x
+        self.y = y
+
+        self.dx = dx
+        self.dy = dy
+
+    def get_x(self, t):
+        return self.x + t * self.dx
+
+    def get_y(self, t):
+        return self.y + t * self.dy
