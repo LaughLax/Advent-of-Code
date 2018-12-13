c0b2b5c7f730fc731c9216234e6c44d977374e9c
David Haralson <d.m.haralson@ieee.org>
2018-12-13 13:24:23 -0700

    Cleaned up code

--- py/2018-13.py
+++ py/2018-13.py
@@ -20,30 +20,9 @@ class AdventOfCode:
         for y, line in enumerate(self.input):
             for x, char in enumerate(line):
                 if char == '/':
-                    if x == 0 or y == 0:
-                        # Top left corner
-                        grid.add_corner(x, y, 1, 1)
-                        
-                    elif line[x-1] == '-' or line[x-1] == '+' or line[x-1] == '<' or line[x-1] == '>' or line[x-1] == '\\':
-                        # Bottom right corner
-                        grid.add_corner(x, y, -1, -1)
-                        
-                    else:
-                        # Top left corner
-                        grid.add_corner(x, y, 1, 1)
-                        
+                    grid.add_corner(x, y, 1)
                 elif char == '\\':
-                    if x == len(line)-1 or y == 0:
-                        # Top right corner
-                        grid.add_corner(x, y, -1, 1)
-                        
-                    elif line[x+1] == '-' or line[x+1] == '+' or line[x+1] == '<' or line[x+1] == '>' or line[x+1] == '/':
-                        # Bottom left corner
-                        grid.add_corner(x, y, -1, 1)
-
-                    else:
-                        # Top right corner
-                        grid.add_corner(x, y, -1, 1)
+                    grid.add_corner(x, y, -1)
 
                 elif char == '+':
                     grid.add_intersection(x, y)
@@ -57,7 +36,7 @@ class AdventOfCode:
                 elif char == 'v':
                     grid.add_cart(Cart(x, y, DIR_DOWN))
 
-                elif char == ' ' or char == '-' or char == '|':
+                elif char in ' -|':
                     pass
                 else:
                     raise Exception(f'Unhandled character! ({char})')
@@ -66,14 +45,14 @@ class AdventOfCode:
 
     def part1(self):
         while not self.grid.wreck_location:
-            self.grid.process_step_pt1()
+            self.grid.process_step()
         return self.grid.wreck_location
     
     def part2(self):
         self.grid = self.make_grid()
 
         while len(self.grid.carts) > 1:
-            self.grid.process_step_pt2()
+            self.grid.process_step()
         
         last_cart = self.grid.carts.pop()
         return (last_cart.x, last_cart.y)
@@ -96,7 +75,7 @@ class Grid:
 
         for corner in self.corners:
             corner_type = self.corners[corner]
-            if corner_type[0] * corner_type[1] == 1:
+            if corner_type == 1:
                 chars[corner[1]][corner[0]] = '/'
             else:
                 chars[corner[1]][corner[0]] = '\\'
@@ -116,8 +95,8 @@ class Grid:
         for line in chars[0:10]:
             print(''.join(line))
 
-    def add_corner(self, x, y, x_dir, y_dir):
-        self.corners[(x, y)] = (x_dir, y_dir)
+    def add_corner(self, x, y, corner_type):
+        self.corners[(x, y)] = corner_type
 
     def add_intersection(self, x, y):
         self.intersections.add((x, y))
@@ -125,63 +104,15 @@ class Grid:
     def add_cart(self, cart):
         self.carts.add(cart)
 
-    def process_step_pt1(self):
+    def process_step(self):
         # Order carts for processing
         carts_ordered = list(sorted(self.carts, key=lambda c: c.y*1000 + c.x))
-        
-        # Process carts:
-        for c in carts_ordered:
-            if c.crash_location:
-                break
-            
-            # Move
-            c.move()
-
-            # Check for collisions
-            for c2 in carts_ordered:
-                if c2 is c:
-                    continue
-                if c.x == c2.x and c.y == c2.y:
-                    c.crash_location = (c.x, c.y)
-                    c2.crash_location = (c.x, c.y)
-                    self.wreck_location = (c.x, c.y)
-                    break
-
-            if self.wreck_location:
-                break
-            
-            # Turn if needed
-            if (c.x, c.y) in self.corners:
-                corner_type = self.corners[(c.x, c.y)]
-                corner_type = corner_type[0] * corner_type[1]
-                if corner_type == 1:
-                    c.direction = {
-                        DIR_RIGHT: DIR_UP,
-                        DIR_UP: DIR_RIGHT,
-                        DIR_LEFT: DIR_DOWN,
-                        DIR_DOWN: DIR_LEFT
-                        }.get(c.direction)
-                elif corner_type == -1:
-                    c.direction = {
-                        DIR_RIGHT: DIR_DOWN,
-                        DIR_DOWN: DIR_RIGHT,
-                        DIR_LEFT: DIR_UP,
-                        DIR_UP: DIR_LEFT
-                        }.get(c.direction)
-            
-            if (c.x, c.y) in self.intersections:
-                c.turn_intersection()
-                
-    def process_step_pt2(self):
-        # Order carts for processing
-        carts_ordered = list(sorted(self.carts, key=lambda c: c.y*1000 + c.x))
-        carts_processed = set()
         
         # Process carts:
         for c in carts_ordered:
             if c.crash_location:
                 continue
-               
+            
             # Move
             c.move()
 
@@ -192,14 +123,14 @@ class Grid:
                 if c.x == c2.x and c.y == c2.y:
                     c.crash_location = (c.x, c.y)
                     c2.crash_location = (c.x, c.y)
+                    self.wreck_location = (c.x, c.y)
                     self.carts.remove(c)
-                    self.carts.remove(c2)
+                    self.carts.remove(c2)                    
                     break
-
+            
             # Turn if needed
             if (c.x, c.y) in self.corners:
                 corner_type = self.corners[(c.x, c.y)]
-                corner_type = corner_type[0] * corner_type[1]
                 if corner_type == 1:
                     c.direction = {
                         DIR_RIGHT: DIR_UP,
@@ -214,7 +145,7 @@ class Grid:
                         DIR_LEFT: DIR_UP,
                         DIR_UP: DIR_LEFT
                         }.get(c.direction)
-                
+            
             if (c.x, c.y) in self.intersections:
                 c.turn_intersection()
                 
@@ -248,8 +179,6 @@ class Cart:
     def turn_intersection(self):
         if self.next_turn == Cart.TURN_LEFT:
             self.direction = (self.direction + 1) % 4
-        elif self.next_turn == Cart.TURN_STRAIGHT:
-            pass
         elif self.next_turn == Cart.TURN_RIGHT:
             self.direction = (self.direction + 3) % 4
 
