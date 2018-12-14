b15e66c50e34a5ee0117c310f51042614fb82a56
David Haralson <d.m.haralson@ieee.org>
2018-12-14 10:59:30 -0700

    Performance improvements 2018-14

--- py/2018-14.py
+++ py/2018-14.py
@@ -1,3 +1,6 @@
+from collections import deque
+
+
 class AdventOfCode:
 
     def __init__(self, filename):
@@ -6,121 +9,56 @@ class AdventOfCode:
 
         self.input = int(self.input_raw)
 
-        self.recipe_list = RecipeList()
-        
-    def part1(self):
-        elf_1 = self.recipe_list.first_recipe
-        elf_2 = self.recipe_list.first_recipe.right
+        self.recipes = [3, 7]
 
-        while self.recipe_list.num_recipes < self.input + 10:
-            new_val = elf_1.value + elf_2.value
+    def part1(self):
+        elf_1 = 0
+        elf_2 = 1
+
+        while len(self.recipes) < self.input + 10:
+            new_val = self.recipes[elf_1] + self.recipes[elf_2]
 
             if new_val >= 10:
-                self.recipe_list.add_recipe(Recipe(1))
-                self.recipe_list.add_recipe(Recipe(new_val % 10))
+                self.recipes.append(1)
+                self.recipes.append(new_val % 10)
             else:
-                self.recipe_list.add_recipe(Recipe(new_val))
+                self.recipes.append(new_val)
 
-            elf_1 = elf_1.move_right(elf_1.value + 1)
-            elf_2 = elf_2.move_right(elf_2.value + 1)
+            elf_1 = (elf_1 + self.recipes[elf_1] + 1) % len(self.recipes)
+            elf_2 = (elf_2 + self.recipes[elf_2] + 1) % len(self.recipes)
 
-        return self.recipe_list.values_after(self.input, 10)
+        return ''.join(str(n) for n in self.recipes[-10:])
 
     
     def part2(self):
-        self.recipe_list = RecipeList()
-        elf_1 = self.recipe_list.first_recipe
-        elf_2 = self.recipe_list.first_recipe.right
+        self.recipes = [3, 7]
+        elf_1 = 0
+        elf_2 = 1
+
+        last_added = deque([-1]*(len(self.input_raw) - 1))
+        check = deque([int(d) for d in self.input_raw])
 
         while True:
-            new_val = elf_1.value + elf_2.value
+            new_val = self.recipes[elf_1] + self.recipes[elf_2]
 
             if new_val >= 10:
-                self.recipe_list.add_recipe(Recipe(1))
-                if self.recipe_list.check_if_sequence_at_end(self.input_raw):
+                self.recipes.append(1)
+                last_added.append(1)
+                if last_added == check:
                     break
-                self.recipe_list.add_recipe(Recipe(new_val % 10))
+                last_added.popleft()
+                
+                self.recipes.append(new_val % 10)
+                last_added.append(new_val % 10)
             else:
-                self.recipe_list.add_recipe(Recipe(new_val))
-            if self.recipe_list.check_if_sequence_at_end(self.input_raw):
+                self.recipes.append(new_val)
+                last_added.append(new_val)
+
+            if last_added == check:
                 break
+            last_added.popleft()
 
-            elf_1 = elf_1.move_right(elf_1.value + 1)
-            elf_2 = elf_2.move_right(elf_2.value + 1)
+            elf_1 = (elf_1 + self.recipes[elf_1] + 1) % len(self.recipes)
+            elf_2 = (elf_2 + self.recipes[elf_2] + 1) % len(self.recipes)
 
-        return self.recipe_list.num_recipes - len(self.input_raw)
-
-
-class RecipeList:
-
-    def __init__(self):
-        self.first_recipe = Recipe(3)
-        Recipe(7).insert_after(self.first_recipe)
-        self.num_recipes = 2
-
-    def add_recipe(self, recipe):
-        recipe.insert_before(self.first_recipe)
-        self.num_recipes += 1
-
-    def values_as_list(self):
-        vals = [self.first_recipe.value]
-        recipe = self.first_recipe.right
-        while recipe is not self.first_recipe:
-            vals.append(recipe.value)
-            recipe = recipe.right
-        return vals
-
-    def values_after(self, after, num):
-        if after >= self.num_recipes:
-            return ''
-
-        added = 0
-        nums = []
-        recipe = self.first_recipe.move_left(self.num_recipes - after)
-        while added < num and recipe is not self.first_recipe:
-            nums.append(str(recipe.value))
-            added += 1
-            recipe = recipe.right
-
-        return ''.join(nums)
-
-    def check_if_sequence_at_end(self, sequence):
-        length = len(sequence)
-        end_seq = self.values_after(self.num_recipes - length, length)
-
-        return True if end_seq == sequence else False
-
-
-class Recipe:
-
-    def __init__(self, value):
-        self.value = value
-        self.left = self
-        self.right = self
-
-    def insert_before(self, other):
-        self.right = other
-        self.left = other.left
-
-        other.left = self
-        self.left.right = self
-
-    def insert_after(self, other):
-        self.left = other
-        self.right = other.right
-
-        other.right = self
-        self.right.left = self
-
-    def move_right(self, n):
-        if n <= 0:
-            return self
-        
-        return self.right.move_right(n - 1)
-        
-    def move_left(self, n):
-        if n <= 0:
-            return self
-        
-        return self.left.move_left(n - 1)
-        
+        return len(self.recipes) - len(self.input_raw)
