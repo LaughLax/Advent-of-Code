7c93e18f176de8ce18e1ec83cdc4f59e6138eaf2
David Haralson <d.m.haralson@ieee.org>
2019-12-20 09:27:55 -0700

    Performance improvements 2019-18

--- py/2019-18.py
+++ py/2019-18.py
@@ -8,47 +8,60 @@ class AdventOfCode:
         with open(filename) as f:
             self.input = f.read().strip().splitlines()
 
-        self.keys = None
-        self.doors = None
-        self.center = None
-        self.key_to_key = None
+        self.key_to_key1 = None
+        self.key_to_key2 = None
 
     def define_graph(self, lines):
-        trimming = True
-        while trimming:
-            trimming = False
-            for y in range(1, len(lines) - 1):
-                for x in range(1, len(lines[y]) - 1):
-                    if lines[y][x] == '#':
-                        continue
+        keys = {}
+        doors = {}
+        center = {}
+        paths = set()
 
-                    wall_neighbors = 0
-                    for d in [(0, -1), (0, 1), (-1, 0), (1, 0)]:
-                        if lines[y+d[1]][x+d[0]] == '#':
-                            wall_neighbors += 1
+        for y in range(1, len(lines) - 1):
+            for x in range(1, len(lines[y]) - 1):
+                c = lines[y][x]
+                if c != '#':
+                    paths.add((x, y))
+                    if c.islower():
+                        keys[c] = (x, y)
+                    elif c.isupper():
+                        doors[c] = (x, y)
+                    elif c in ('@', '0', '1', '2', '3'):
+                        center[c] = (x, y)
 
-                    if wall_neighbors == 3 and not lines[y][x].islower() and lines[y][x] not in ('@', '0', '1', '2', '3'):
-                        lines[y][x] = '#'
-                        trimming = True
+        to_remove = set()
+        for x, y in paths:
+            if lines[y][x] in keys or lines[y][x] in center or (x, y) in to_remove:
+                continue
+
+            path_neighbors = []
+            for d in ((0, -1), (0, 1), (-1, 0), (1, 0)):
+                if (x+d[0], y+d[1]) in paths:
+                    path_neighbors.append((x, y))
+
+            if len(path_neighbors) == 1:
+                to_remove.add((x, y))
+        while len(to_remove) > 0:
+            x, y = to_remove.pop()
+            if lines[y][x] in keys or lines[y][x] in center:
+                continue
+
+            path_neighbors = []
+            for d in ((0, -1), (0, 1), (-1, 0), (1, 0)):
+                if (x+d[0], y+d[1]) in paths:
+                    path_neighbors.append((x+d[0], y+d[1]))
+
+            if len(path_neighbors) == 1:
+                lines[y][x] = '#'
+                paths.remove((x, y))
+                to_remove.add(path_neighbors[0])
 
         # maze_map = '\n'.join([''.join(line) for line in lines])
         # print(maze_map)
 
         G = nx.Graph()
-        self.keys = {}
-        self.doors = {}
-        self.center = {}
-        for y in range(len(lines)):
-            for x in range(len(lines[y])):
-                c = lines[y][x]
-                if c != '#':
-                    G.add_node((x, y), name=c)
-                    if c.islower():
-                        self.keys[c] = (x, y)
-                    elif c.isupper():
-                        self.doors[c] = (x, y)
-                    elif c in ('@', '0', '1', '2', '3'):
-                        self.center[c] = (x, y)
+        for x, y in paths:
+            G.add_node((x, y))
 
         for node in G:
             for direction in ((0, -1), (0, 1), (-1, 0), (1, 0)):
@@ -56,97 +69,87 @@ class AdventOfCode:
                 if neighbor in G.nodes:
                     G.add_edge(node, neighbor)
 
-        self.key_to_key = {}
-        for key1 in list(self.keys):
-            key1_d = self.key_to_key.setdefault(key1, {})
-            for key2 in self.keys:
+        key_to_key = {}
+        for key1 in list(keys):
+            key1_d = key_to_key.setdefault(key1, {})
+            for key2 in keys:
                 if key2 is key1:
                     continue
 
-                if nx.has_path(G, self.keys[key1], self.keys[key2]):
-                    path = nx.shortest_path(G, self.keys[key1], self.keys[key2])
+                if nx.has_path(G, keys[key1], keys[key2]):
+                    path = nx.shortest_path(G, keys[key1], keys[key2])
                     my_len = len(path) - 1
-                    doors = set()
-                    for door in self.doors:
-                        if self.doors[door] in path:
-                            doors.add(door)
+                    path_doors = set()
+                    for door in doors:
+                        if doors[door] in path:
+                            path_doors.add(door.lower())
 
-                    key1_d[key2] = (my_len, doors)
+                    key1_d[key2] = (my_len, path_doors)
 
-                    key2_d = self.key_to_key.setdefault(key2, {})
-                    key2_d[key1] = (my_len, doors)
+                    key2_d = key_to_key.setdefault(key2, {})
+                    key2_d[key1] = (my_len, path_doors)
 
-        for key1 in self.center:
-            key1_d = self.key_to_key.setdefault(key1, {})
-            for key2 in self.keys:
+        for key1 in center:
+            key1_d = key_to_key.setdefault(key1, {})
+            for key2 in keys:
                 if key2 is key1:
                     continue
 
-                if nx.has_path(G, self.center[key1], self.keys[key2]):
-                    path = nx.shortest_path(G, self.center[key1], self.keys[key2])
+                if nx.has_path(G, center[key1], keys[key2]):
+                    path = nx.shortest_path(G, center[key1], keys[key2])
                     my_len = len(path) - 1
-                    doors = set()
-                    for door in self.doors:
-                        if self.doors[door] in path:
-                            doors.add(door)
+                    path_doors = set()
+                    for door in doors:
+                        if doors[door] in path:
+                            path_doors.add(door.lower())
 
-                    key1_d[key2] = (my_len, doors)
+                    key1_d[key2] = (my_len, path_doors)
 
-                    key2_d = self.key_to_key.setdefault(key2, {})
-                    key2_d[key1] = (my_len, doors)
+                    key2_d = key_to_key.setdefault(key2, {})
+                    key2_d[key1] = (my_len, path_doors)
+
+        return key_to_key
 
     def part1(self):
         lines = [[c for c in line] for line in self.input]
-        self.define_graph(lines)
+        self.key_to_key1 = self.define_graph(lines)
 
         memo = {}
 
-        states_to_examine = [(0, (frozenset(),
-                                  '@',
-                                  0))]
+        states_to_examine = [(0, (frozenset(), '@'))]
         heapq.heapify(states_to_examine)
 
-        winner_state = None
-        parents = {}
-
-        while winner_state is None:
+        while True:
             cdist, state = heapq.heappop(states_to_examine)
+            keys, pos = state
 
-            if len(state[0]) == 1 + 26:
-                winner_state = state
+            if len(keys) == 1 + 26:
+                break
 
-            for next_key in self.key_to_key[state[1]]:
-                if next_key in state[0]:
+            for next_key in self.key_to_key1[pos]:
+                if next_key in keys:
                     continue
 
-                dist, doors = self.key_to_key[state[1]][next_key]
+                dist, doors = self.key_to_key1[pos][next_key]
 
                 key_failed = False
                 for d in doors:
-                    if d.lower() not in state[0]:
+                    if d not in keys:
                         key_failed = True
+                        break
                 if key_failed:
                     continue
 
-                next_layer_keys = state[0] | frozenset(next_key)
+                next_layer_dist = cdist + dist
+                next_layer_keys = keys | frozenset(next_key)
+                next_state = (next_layer_keys,
+                              next_key)
 
-                next_layer_dist = state[2] + dist
-                if (next_layer_keys, next_key) not in memo or next_layer_dist < memo[(next_layer_keys, next_key)]:
-                    memo[(next_layer_keys, next_key)] = next_layer_dist
-                    next_state = (next_layer_keys,
-                                  next_key,
-                                  next_layer_dist)
-                    parents[(next_state[0], next_state[1])] = (state[0], state[1])
+                if next_state not in memo or next_layer_dist < memo[next_state]:
+                    memo[next_state] = next_layer_dist
                     heapq.heappush(states_to_examine, (next_layer_dist, next_state))
 
-        # s = (winner_state[0], winner_state[1])
-        # sequence = [s[1]]
-        # while s in parents:
-        #     s = parents[s]
-        #     sequence.append(s[1])
-        # sequence.reverse()
-
-        return winner_state[2]
+        return cdist
 
     def part2(self):
         lines = [[c for c in line] for line in self.input]
@@ -161,58 +164,44 @@ class AdventOfCode:
         lines[c_y+1][c_x-1] = '2'
         lines[c_y+1][c_x] = '#'
         lines[c_y+1][c_x+1] = '3'
-        self.define_graph(lines)
+        self.key_to_key2 = self.define_graph(lines)
 
         memo = {}
 
-        states_to_examine = [(0, (frozenset(('0', '1', '2', '3')),
-                                  ('0', '1', '2', '3'),
-                                  0))]
+        states_to_examine = [(0, (frozenset(('0', '1', '2', '3')), ('0', '1', '2', '3')))]
         heapq.heapify(states_to_examine)
 
-        winner_state = None
-        parents = {}
-
-        while winner_state is None:
+        while True:
             cdist, state = heapq.heappop(states_to_examine)
+            keys, pos = state
 
-            if len(state[0]) == 4+26:
-                winner_state = state
+            if len(keys) == 4+26:
+                break
 
             for i in range(4):
-                for next_key in self.key_to_key[state[1][i]]:
-                    if next_key in state[0]:
+                for next_key in self.key_to_key2[pos[i]]:
+                    if next_key in keys:
                         continue
 
-                    dist, doors = self.key_to_key[state[1][i]][next_key]
+                    dist, doors = self.key_to_key2[pos[i]][next_key]
 
                     key_failed = False
                     for d in doors:
-                        if d.lower() not in state[0]:
+                        if d not in keys:
                             key_failed = True
+                            break
                     if key_failed:
                         continue
 
-                    next_layer_keys = state[0] | frozenset(next_key)
-                    next_pos = []
-                    for j in range(4):
-                        next_pos.append(state[1][j] if j != i else next_key)
-                    next_pos = tuple(next_pos)
+                    next_layer_keys = keys | frozenset(next_key)
+                    next_pos = tuple(pos[j] if j != i else next_key for j in range(4))
 
                     next_layer_dist = cdist + dist
-                    if (next_layer_keys, next_pos) not in memo or next_layer_dist < memo[(next_layer_keys, next_pos)]:
-                        memo[(next_layer_keys, next_pos)] = next_layer_dist
-                        next_state = (next_layer_keys,
-                                      next_pos,
-                                      next_layer_dist)
-                        parents[(next_state[0], next_state[1])] = (state[0], state[1])
+                    next_state = (next_layer_keys,
+                                  next_pos)
+
+                    if next_state not in memo or next_layer_dist < memo[next_state]:
+                        memo[next_state] = next_layer_dist
                         heapq.heappush(states_to_examine, (next_layer_dist, next_state))
 
-        # s = (winner_state[0], winner_state[1])
-        # sequence = [s[1]]
-        # while s in parents:
-        #     s = parents[s]
-        #     sequence.append(s[1])
-        # sequence.reverse()
-
-        return winner_state[2]
+        return cdist
