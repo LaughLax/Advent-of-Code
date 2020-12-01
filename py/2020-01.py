d33dc28e8bf622fae9576a13129091502a916029
David Haralson <d.m.haralson@ieee.org>
2020-11-30 22:33:37 -0700

    Performance improvements 2020-01

--- py/2020-01.py
+++ py/2020-01.py
@@ -4,22 +4,31 @@ class AdventOfCode:
         with open(filename) as f:
             self.input = f.read().splitlines()
 
-            self.input = list(map(int, self.input))
+            self.input = sorted(list(map(int, self.input)))
 
     def part1(self):
         for i in range(len(self.input)):
+            num_i = self.input[i]
             for j in range(i, len(self.input)):
-                if self.input[i] + self.input[j] == 2020:
-                    print(self.input[i])
-                    print(self.input[j])
-                    return self.input[i] * self.input[j]
+                num_j = self.input[j]
+
+                if num_i + num_j == 2020:
+                    return num_i * num_j
+
+                elif num_i + num_j > 2020:
+                    break
 
     def part2(self):
         for i in range(len(self.input)):
+            num_i = self.input[i]
             for j in range(i+1, len(self.input)):
+                num_j = self.input[j]
+                if num_i + num_j > 2020:
+                    break
                 for k in range(j+1, len(self.input)):
-                    if self.input[i] + self.input[j] + self.input[k] == 2020:
-                        print(self.input[i])
-                        print(self.input[j])
-                        print(self.input[k])
-                        return self.input[i] * self.input[j] * self.input[k]
+                    num_k = self.input[k]
+
+                    if num_i + num_j + num_k == 2020:
+                        return num_i * num_j * num_k
+                    elif num_i + num_j + num_k > 2020:
+                        break
