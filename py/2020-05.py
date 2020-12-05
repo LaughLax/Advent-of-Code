e542ed65c7415320df51ff9a482dfb5af8713d67
David Haralson <d.m.haralson@ieee.org>
2020-12-04 22:35:37 -0700

    Change approach + optimize

--- py/2020-05.py
+++ py/2020-05.py
@@ -2,41 +2,17 @@ class AdventOfCode:
 
     def __init__(self, filename):
         with open(filename) as f:
-            self.input = f.read().splitlines()
-
-    def parse_line(self, line):
-        row = 0
-        for char in line[0:7]:
-            row = row << 1
-            if char == 'B':
-                row += 1
-        col = 0
-        for char in line[7:]:
-            col = col << 1
-            if char == 'R':
-                col += 1
-        return row * 8 + col
+            self.input = f.read() \
+                .replace('F', '0').replace('B', '1') \
+                .replace('L', '0').replace('R', '1') \
+                .splitlines()
+            self.seat_ids = set([int(line, base=2) for line in self.input])
 
     def part1(self):
-        max_id = 0
-        for line in self.input:
-            seat_id = self.parse_line(line)
-            max_id = max(max_id, seat_id)
-
-        return max_id
+        return max(self.seat_ids)
 
     def part2(self):
-        ids = set()
-        min_id = 10000
-        max_id = 0
-        for line in self.input:
-            seat_id = self.parse_line(line)
-            ids.add(seat_id)
-            min_id = min(min_id, seat_id)
-            max_id = max(max_id, seat_id)
-
-        all_ids = set(range(min_id,max_id+1))
-        reduced = all_ids ^ ids
-        for seat in reduced:
-            if min_id < seat < max_id:
-                return seat
+        min_id = min(self.seat_ids)
+        max_id = max(self.seat_ids)
+        all_ids = set(range(min_id, max_id+1))
+        return (all_ids - self.seat_ids).pop()
