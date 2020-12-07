aeb32f9746d749a5ff520fd3cd94b8137a1cba51
David Haralson <d.m.haralson@ieee.org>
2020-12-06 22:57:49 -0700

    Clean & consolidate code

--- py/2020-07.py
+++ py/2020-07.py
@@ -4,8 +4,7 @@ import re
 
 class Tree:
 
-    def __init__(self, description, color):
-        self.desc = description
+    def __init__(self, color):
         self.color = color
         self.parents = set()
         self.children = dict()
@@ -15,7 +14,7 @@ class Tree:
         tree.children[self] = int(child_count)
 
     def __str__(self):
-        return f'{self.desc} {self.color}'
+        return self.color
 
     def contains(self):
         t = 0
@@ -30,46 +29,32 @@ class AdventOfCode:
         with open(filename) as f:
             self.input = f.read().splitlines()
 
-    def part1(self):
-        bags = dict()
-        pattern = re.compile('(\d+)\s+(\w+)\s+(\w+)\s+bags?')
+        self.bags = dict()
+        self.parse_input()
+
+    def parse_input(self):
+        pattern = re.compile(r'(\d+) (\w+ \w+) bags?')
         for line in self.input:
             bag_type, _, remainder = line.partition(' bags ')
             children = pattern.findall(remainder)
-            if bag_type not in bags:
-                desc, color = bag_type.split(' ')
-                bags[bag_type] = Tree(desc, color)
+            if bag_type not in self.bags:
+                self.bags[bag_type] = Tree(bag_type)
             for c in children:
-                name = f'{c[1]} {c[2]}'
-                if name not in bags:
-                    bags[name] = Tree(c[1], c[2])
-                bags[name].add_parent(bags[bag_type], c[0])
+                if c[1] not in self.bags:
+                    self.bags[c[1]] = Tree(c[1])
+                self.bags[c[1]].add_parent(self.bags[bag_type], c[0])
 
+    def part1(self):
         q = 'shiny gold'
-        parents = set(bags[q].parents)
-        unexplored = set(parents)
-        explored = set()
+        unexplored = set(self.bags[q].parents)
+        parents = set()
         while len(unexplored) > 0:
             bag = unexplored.pop()
             parents.add(bag)
-            explored.add(bag)
             for b in bag.parents:
-                if b not in explored:
+                if b not in parents:
                     unexplored.add(b)
         return len(parents)
 
     def part2(self):
-        bags = dict()
-        pattern = re.compile('(\d+)\s+(\w+)\s+(\w+)\s+bags?')
-        for line in self.input:
-            bag_type, _, remainder = line.partition(' bags ')
-            children = pattern.findall(remainder)
-            if bag_type not in bags:
-                desc, color = bag_type.split(' ')
-                bags[bag_type] = Tree(desc, color)
-            for c in children:
-                name = f'{c[1]} {c[2]}'
-                if name not in bags:
-                    bags[name] = Tree(c[1], c[2])
-                bags[name].add_parent(bags[bag_type], c[0])
-        return bags['shiny gold'].contains()
+        return self.bags['shiny gold'].contains()
