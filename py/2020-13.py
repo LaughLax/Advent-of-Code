bd5ccf12a46a68ae5df8e5bd536893a4f8abd7af
David Haralson <d.m.haralson@ieee.org>
2020-12-12 22:57:31 -0700

    Minor cleanup and performance improvements

--- py/2020-13.py
+++ py/2020-13.py
@@ -7,45 +7,35 @@ class AdventOfCode:
         with open(filename) as f:
             self.input = f.read().splitlines()
 
+        buses = self.input[1].split(',')
+        self.times = []
+        for i, bus in enumerate(buses):
+            if bus != 'x':
+                self.times.append((i, int(bus)))
+
     def part1(self):
         t0 = int(self.input[0])
-        buses = self.input[1].split(',')
-        times = []
-        for bus in buses:
-            if bus != 'x':
-                times.append(int(bus))
 
         min_wait = 1_000_000_000
         min_bus = 0
-        for bus in times:
-            t = bus - (t0 % bus) if t0 % bus > 0 else 0
+        for bus in self.times:
+            t = bus[1] - (t0 % bus[1]) if t0 % bus[1] > 0 else 0
             if t < min_wait:
                 min_wait = t
-                min_bus = bus
+                min_bus = bus[1]
 
         return min_wait*min_bus
 
     def part2(self):
-        buses = self.input[1].split(',')
-        times = []
         n = 0
-        for i, bus in enumerate(buses):
-            if bus != 'x':
-                times.append((i, int(bus)))
-
-        k = 0
-        n = 0
-        interval = 1
-        found_k = dict()
-        all_t = frozenset(times[1:])
-        while len(found_k.keys()) < len(all_t):
-            k += interval
-            n = times[0][1] * k
-            for bus in times[1:]:
-                if bus in found_k:
-                    continue
+        interval = self.times[0][1]
+        all_t = frozenset(self.times[1:])
+        found_t = set()
+        while len(found_t) < len(all_t):
+            n += interval
+            for bus in all_t - found_t:
                 if (n+bus[0]) % bus[1] == 0:
-                    found_k[bus] = k
+                    found_t.add(bus)
                     interval *= bus[1]
 
         return n
