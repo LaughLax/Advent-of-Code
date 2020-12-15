d355dda8132a326804de101e91f85d173b0e22a3
David Haralson <d.m.haralson@ieee.org>
2020-12-14 22:52:52 -0700

    Consolidate parts into one common function

--- py/2020/15.py
+++ py/2020/15.py
@@ -4,48 +4,29 @@ class AdventOfCode:
         with open(filename) as f:
             self.input = [int(val) for val in f.read().strip().split(',')]
 
-    def part1(self):
-        turn = len(self.input)
-        rounds = self.input.copy()
-
-        most_recent = dict()
-        for i, val in enumerate(rounds):
-            if i == len(rounds) - 1:
-                break
-            most_recent[val] = i+1
-
-        while turn < 2020:
-            turn += 1
-
-            if rounds[-1] in most_recent:
-                next_num = turn - most_recent[rounds[-1]] - 1
-            else:
-                next_num = 0
-
-            most_recent[rounds[-1]] = turn - 1
-            rounds.append(next_num)
-
-        return rounds[2019]
-
-    def part2(self):
+    def run_part(self, stop_at):
         turn = len(self.input)
 
-        most_recent = dict()
-        for i, val in enumerate(self.input):
-            if i == len(self.input) - 1:
-                break
-            most_recent[val] = i+1
+        recent = dict()
+        for i, val in enumerate(self.input[:-1]):
+            recent[val] = i + 1
         last_val = self.input[-1]
 
-        while turn < 30_000_000:
-            turn += 1
-
-            if last_val in most_recent:
-                next_num = turn - most_recent[last_val] - 1
+        next_num = None
+        while turn < stop_at:
+            if last_val in recent:
+                next_num = turn - recent[last_val]
             else:
                 next_num = 0
 
-            most_recent[last_val] = turn - 1
+            recent[last_val] = turn
             last_val = next_num
+            turn += 1
 
         return next_num
+
+    def part1(self):
+        return self.run_part(2020)
+
+    def part2(self):
+        return self.run_part(30_000_000)
