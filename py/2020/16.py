21872ebd8109d5c6271c8f9f78f38cfebd212440
David Haralson <d.m.haralson@ieee.org>
2020-12-15 23:00:50 -0700

    Clean, consolidate, refactor, and ever so slightly optimize

--- py/2020/16.py
+++ py/2020/16.py
@@ -4,11 +4,20 @@ class AdventOfCode:
         with open(filename) as f:
             self.input = f.read()
 
-        self.fields, split, rem = self.input.partition('\n\n')
-        self.fields = self.fields.splitlines()
+        fields, split, rem = self.input.partition('\n\n')
+        fields = fields.splitlines()
+
+        self.fields = dict()
+        for f in fields:
+            fname, _, nums = f.partition(': ')
+            nums_1, nums_2 = nums.split(' or ')
+            min_1, max_1 = nums_1.split('-')
+            min_2, max_2 = nums_2.split('-')
+            self.fields[fname] = tuple([int(v) for v in (min_1, max_1, min_2, max_2)])
+
         rem = rem.splitlines()
-        self.my_ticket = [int(v) for v in rem[1].split(',')]
-        self.tickets = rem[4:]
+        self.my_ticket = tuple([int(v) for v in rem[1].split(',')])
+        self.tickets = tuple(tuple([int(v) for v in line.split(',')]) for line in rem[4:])
 
         self.valid = set()
 
@@ -16,73 +25,59 @@ class AdventOfCode:
         allowable = set()
         minimum = 0
         maximum = 1_000_000
-        for line in self.fields:
-            fname, _, nums = line.partition(': ')
-            nums_1, nums_2 = nums.split(' or ')
-            min_1, max_1 = nums_1.split('-')
-            min_2, max_2 = nums_2.split('-')
-            min_1, max_1, min_2, max_2 = [int(v) for v in (min_1, max_1, min_2, max_2)]
+        for f_name in self.fields:
+            f_vals = self.fields[f_name]
+            if f_vals[0] < minimum:
+                minimum = f_vals[0]
+            if f_vals[1] < minimum:
+                minimum = f_vals[1]
+            if f_vals[2] > maximum:
+                maximum = f_vals[2]
+            if f_vals[3] > maximum:
+                maximum = f_vals[3]
 
-            if min_1 < minimum:
-                minimum = min_1
-            if min_2 < minimum:
-                minimum = min_2
-            if max_1 > maximum:
-                maximum = max_1
-            if max_2 > maximum:
-                maximum = max_2
-
-            for i1 in range (min_1, max_1+1):
+            for i1 in range(f_vals[0], f_vals[1]+1):
                 allowable.add(i1)
-            for i2 in range (min_2, max_2+1):
+            for i2 in range(f_vals[2], f_vals[3]+1):
                 allowable.add(i2)
 
         error = 0
         for ticket in self.tickets:
-            vals_list = tuple(int(v) for v in ticket.split(','))
-            vals = set(vals_list)
-            bad = vals - allowable
+            t_vals = set(ticket)
+            bad = t_vals - allowable
             if len(bad) > 0:
                 error += sum(bad)
             else:
-                self.valid.add(vals_list)
+                self.valid.add(ticket)
 
         return error
 
     def part2(self):
-        f1 = dict()
-        f2 = dict()
-        for line in self.fields:
-            fname, _, nums = line.partition(': ')
-            nums_1, nums_2 = nums.split(' or ')
-            min_1, max_1 = nums_1.split('-')
-            min_2, max_2 = nums_2.split('-')
-            f1[fname] = (int(min_1), int(max_1))
-            f2[fname] = (int(min_2), int(max_2))
-
         possible = dict()
-        for field in f1:
-            possible[field] = list(range(len(f1)))
+        for field in self.fields:
+            possible[field] = list(range(len(self.fields)))
 
         for ticket in self.valid:
             for field in possible:
-                for i, val in enumerate(ticket):
-                    if i in possible[field]:
-                        if (f1[field][0] > val or f1[field][1] < val)\
-                                and (f2[field][0] > val or f2[field][1] < val):
-                            possible[field].remove(i)
+                to_check = possible[field]
+                limits = self.fields[field]
 
-        possibilities = sorted(possible.keys(), key=lambda k: len(possible[k]))
-        established = set()
-        for f in possibilities:
-            for k in established:
+                for i, t_val in enumerate(ticket):
+                    if i in to_check:
+                        if not ((limits[0] <= t_val <= limits[1])
+                                or (limits[2] <= t_val <= limits[3])):
+                            to_check.remove(i)
+
+        ordered = sorted(possible.keys(), key=lambda f_name: len(possible[f_name]))
+        known = set()
+        prod = 1
+        for f in ordered:
+            for k in known:
                 possible[f].remove(possible[k][0])
-            established.add(f)
-
-        mult = 1
-        for f in possible:
+            known.add(f)
             if f.startswith('departure'):
-                mult *= self.my_ticket[possible[f][0]]
-        return mult
+                prod *= self.my_ticket[possible[f][0]]
+
+        return prod
 
 
