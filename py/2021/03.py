9af57e6db8839fb14ba13927c8b406ad8076ee46
David Haralson <d.m.haralson@ieee.org>
2021-12-02 23:21:49 -0700

    Less casting, more direct operations

--- py/2021/03.py
+++ py/2021/03.py
@@ -6,49 +6,42 @@ class AdventOfCode:
     def __init__(self, filename):
         with open(filename) as f:
             self.input = f.read().splitlines()
-            # self.input = [int(i, base=2) for i in self.input]
+            self.num_size = len(self.input[0])
+            self.bits = [[i == '1' for i in line] for line in self.input]
 
     def part1(self):
-        bits = [[int(i) for i in line] for line in self.input]
-        bits = np.array(bits)
-        ct = np.sum(bits, axis=0)
-        gamma_bits = [str(int(c >= len(self.input)/2)) for c in ct]
-        eps_bits = [str(1-int(c)) for c in gamma_bits]
-        gamma = int(''.join(gamma_bits), base=2)
-        eps = int(''.join(eps_bits), base=2)
+        ct = np.sum(self.bits, axis=0) >= len(self.input)/2
+        gamma = eps = 0
+        for i, val in enumerate(ct):
+            gamma += val * (1 << (self.num_size-i-1))
+            eps += (not val) * (1 << (self.num_size-i-1))
         return gamma * eps
 
     def part2(self):
-        bits = [[int(i) for i in line] for line in self.input]
-
         # Oxygen
-        bits_oxy = bits.copy()
-        for i in range(12):
+        bits_oxy = self.bits.copy()
+        for i in range(self.num_size):
             bits_copy = bits_oxy.copy()
-            want = int(np.sum(bits_copy, axis=0)[i] >= len(bits_copy)/2)
+            want = np.sum(np.array(bits_copy)[:, i]) >= len(bits_copy)/2
             for row in bits_oxy:
                 if row[i] != want:
                     bits_copy.remove(row)
-                if len(bits_copy) == 1:
-                    break
             if len(bits_copy) == 1:
                 break
             bits_oxy = bits_copy
-        oxygen = int(''.join([str(c) for c in bits_copy.pop()]), base=2)
+        oxygen = np.sum([val * (1 << (self.num_size-i-1)) for i, val in enumerate(bits_copy.pop())])
 
         # CO2
-        bits_co2 = bits.copy()
-        for i in range(12):
+        bits_co2 = self.bits.copy()
+        for i in range(self.num_size):
             bits_copy = bits_co2.copy()
-            want = int(np.sum(bits_copy, axis=0)[i] < len(bits_copy)/2)
+            want = np.sum(np.array(bits_copy)[:, i]) < len(bits_copy)/2
             for row in bits_co2:
                 if row[i] != want:
                     bits_copy.remove(row)
-                if len(bits_copy) == 1:
-                    break
             if len(bits_copy) == 1:
                 break
             bits_co2 = bits_copy
-        co2 = int(''.join([str(c) for c in bits_copy.pop()]), base=2)
+        co2 = np.sum([val * (1 << (self.num_size-i-1)) for i, val in enumerate(bits_copy.pop())])
 
         return oxygen * co2
