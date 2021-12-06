afd4c19db95a96c7e255fbf81cbf3e16831ee9df
David Haralson <david.haralson@keycaptureenergy.com>
2021-12-06 10:19:49 -0700

    Slim down code slightly

--- py/2021/06.py
+++ py/2021/06.py
@@ -5,10 +5,10 @@ class AdventOfCode:
 
     def __init__(self, filename):
         with open(filename) as f:
-            self.input = list(map(int, f.read().split(',')))
+            self.input = np.array(list(map(int, f.read().split(','))))
 
     def part1(self):
-        fish = np.array(self.input)
+        fish = self.input.copy()
         for day in range(80):
             new = np.sum(fish == 0)
             fish = np.append(fish, [9]*new)
@@ -16,11 +16,10 @@ class AdventOfCode:
         return len(fish)
 
     def part2(self):
-        fish_array = np.array(self.input)
         fish = {}
         for i in range(9):
-            fish[i] = np.sum(fish_array == i, dtype=np.ulonglong)
-        for day in range(256):
+            fish[i] = np.sum(self.input == i, dtype=np.ulonglong)
+        for _ in range(256):
             new = fish[0]
             for i in range(8):
                 fish[i] = fish[i+1]
