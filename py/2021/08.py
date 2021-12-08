d3f3159b5cd65d49317d1fb3d272a712f924f054
David Haralson <d.m.haralson@ieee.org>
2021-12-07 23:07:12 -0700

    Reduce extra code

--- py/2021/08.py
+++ py/2021/08.py
@@ -1,9 +1,3 @@
-def match_map(decoder, val):
-    for i, v in enumerate(decoder):
-        if val == v:
-            return i
-
-
 class AdventOfCode:
 
     def __init__(self, filename):
@@ -32,54 +26,42 @@ class AdventOfCode:
         # 4 = len(4)
         # 8 = len(7)
         # 9 = len(6) with all of 4
-        # 6 = len(6) missing one from 1
-        # 0 = remaining len(6)
+        # 0 = remaining len(6) with all of 1
+        # 6 = remaining len(6)
         # 5 = len(5) fully in 6
         # 3 = remaining len(5) fully in 9
         # 2 = remaining len(5) missing one from 1
 
         total = 0
+        nums = [None]*10
         for i in range(len(self.input)):
             dec = [set(n) for n in self.decode[i]]
             outp = [set(n) for n in self.output[i]]
-            n1 = next(filter(lambda x: len(x) == 2, dec))
-            dec.remove(n1)
-            n7 = next(filter(lambda x: len(x) == 3, dec))
-            dec.remove(n7)
-            n4 = next(filter(lambda x: len(x) == 4, dec))
-            dec.remove(n4)
-            n8 = next(filter(lambda x: len(x) == 7, dec))
-            dec.remove(n8)
-            n9 = next(filter(lambda x: (len(x) == 6) and n4.issubset(x), dec))
-            dec.remove(n9)
-            n0 = next(filter(lambda x: (len(x) == 6) and n1.issubset(x), dec))
-            dec.remove(n0)
-            n6 = next(filter(lambda x: len(x) == 6, dec))
-            dec.remove(n6)
-            n5 = next(filter(lambda x: (len(x) == 5) and x.issubset(n6), dec))
-            dec.remove(n5)
-            n3 = next(filter(lambda x: (len(x) == 5) and x.issubset(n9), dec))
-            dec.remove(n3)
-            n2 = dec.pop()
 
-            n_map = [
-                n0,
-                n1,
-                n2,
-                n3,
-                n4,
-                n5,
-                n6,
-                n7,
-                n8,
-                n9,
-            ]
+            nums[1] = next(filter(lambda x: len(x) == 2, dec))
+            dec.remove(nums[1])
+            nums[7] = next(filter(lambda x: len(x) == 3, dec))
+            dec.remove(nums[7])
+            nums[4] = next(filter(lambda x: len(x) == 4, dec))
+            dec.remove(nums[4])
+            nums[8] = next(filter(lambda x: len(x) == 7, dec))
+            dec.remove(nums[8])
+            nums[9] = next(filter(lambda x: (len(x) == 6) and nums[4].issubset(x), dec))
+            dec.remove(nums[9])
+            nums[0] = next(filter(lambda x: (len(x) == 6) and nums[1].issubset(x), dec))
+            dec.remove(nums[0])
+            nums[6] = next(filter(lambda x: len(x) == 6, dec))
+            dec.remove(nums[6])
+            nums[5] = next(filter(lambda x: (len(x) == 5) and x.issubset(nums[6]), dec))
+            dec.remove(nums[5])
+            nums[3] = next(filter(lambda x: (len(x) == 5) and x.issubset(nums[9]), dec))
+            dec.remove(nums[3])
+            nums[2] = dec.pop()
 
-            val = (1_000 * match_map(n_map, outp[0])
-                   + 100 * match_map(n_map, outp[1])
-                   + 10 * match_map(n_map, outp[2])
-                   + match_map(n_map, outp[3]))
+            val = (1_000 * nums.index(outp[0])
+                   + 100 * nums.index(outp[1])
+                   + 10 * nums.index(outp[2])
+                   + nums.index(outp[3]))
             total += val
 
         return total
-
